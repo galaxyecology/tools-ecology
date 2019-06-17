de114e598c562a90b5f6f3f0fb8970bc070470f0
Alanamosse <alan.amosse@gmail.com>
2019-06-17 16:17:21 +0200

    Try to use Cairo to fix type

--- tools/regionalgam/autocorr-res-acf.R
+++ tools/regionalgam/autocorr-res-acf.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
-
+library("Cairo")
 args = commandArgs(trailingOnly=TRUE)
 load(args[1])
 
-png('output-acf.png',width = 480, height = 480, units = "px")
+png('output-acf.png',width = 480, height = 480, units = "px",type="cairo",antialias="default")
 graph<-acf(residuals(mod,type="normalized"),plot=FALSE)
 plot(graph, main="Acf residuals")
 invisible(dev.off())
