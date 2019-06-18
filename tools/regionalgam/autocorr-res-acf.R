22de3b07508f123140d52caba245adb4ed3ea78e
Alanamosse <alanamosse@60-E3-27-12-8C-3F.home>
2019-06-18 14:39:11 +0200

    add regionalgam tools

---
+++ tools/regionalgam/autocorr-res-acf.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+load(args[1])
+
+png('output-acf.png',width = 480, height = 480, units = "px")
+graph<-acf(residuals(mod,type="normalized"),plot=FALSE)
+plot(graph, main="Acf residuals")
+invisible(dev.off())
+sink("output-acf.txt")
+print(graph)
---
+++ tools/regionalgam/plot-trend.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+library(nlme)
+library(MASS)
+
+args = commandArgs(trailingOnly=TRUE)
+input = read.table(args[1], header=TRUE,sep="	") #input=data.index =ab_index-output
+load(args[2])
+
+glmm.mod_fullyear <- glmmPQL(regional_gam~ as.factor(YEAR)-1,data=input,family=quasipoisson,random=~1|SITE, correlation = corAR1(form = ~ YEAR | SITE),verbose = FALSE)
+
+col.index <- as.numeric(glmm.mod_fullyear$coefficients$fixed)
+year <- unique(input$YEAR)
+
+png('output-plot-trend.png')
+plot(year,col.index, type='o',xlab="year",ylab="collated index")
+abline(mod,lty=2,col="red")
+invisible(dev.off())
+
+d<-data.frame(year,col.index)
+colnames(d)<-c("year","collated index")
+write.table(d,"s_trend.tsv",sep="	",row.names=FALSE)
+
+cat("Show trend line on abundance plot")
