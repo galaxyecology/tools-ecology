74027957b82523eb9474449b2a89bd5f2ad9989b
sbenateau <sbenateau@mailodie.fr>
2019-07-16 14:30:13 +0200

    add fread for abundance as weell

--- tools/regionalgam/ab-index.R
+++ tools/regionalgam/ab-index.R
@@ -4,8 +4,10 @@ args = commandArgs(trailingOnly=TRUE)
 source(args[1])
 
 
-tryCatch({input = read.table(args[2], header=TRUE,sep=" ")},finally={input = read.table(args[2], header=TRUE,sep=",")})
-pheno = read.table(args[3], header=TRUE,sep="	")
+# import file
+input = fread(args[2])
+# retransform to data.frame to allow the calculation by the fonctions from the package
+input = data.frame(input)
 
 if("TREND" %in% colnames(input)){
     input <- input[input$TREND==1,c("SPECIES","SITE","YEAR","MONTH","DAY","COUNT")]
