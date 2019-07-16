a90baf042adf36c92fbd60df056376b7de83b071
sbenateau <sbenateau@mailodie.fr>
2019-07-16 15:09:36 +0200

    bring back pheno and try to make comment clearer

--- tools/regionalgam/ab-index.R
+++ tools/regionalgam/ab-index.R
@@ -6,8 +6,9 @@ source(args[1])
 
 # import file
 input = fread(args[2])
-# retransform to data.frame to allow the calculation by the fonctions from the package
+# retransform to data.frame to allow the calculation by the fonctions in the file dennis-gam-initial-function.R
 input = data.frame(input)
+pheno = read.table(args[3], header=TRUE,sep="	")
 
 if("TREND" %in% colnames(input)){
     input <- input[input$TREND==1,c("SPECIES","SITE","YEAR","MONTH","DAY","COUNT")]
