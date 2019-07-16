4ab01e3a05ff4cead5c20895f61ab7f940a9151b
sbenateau <sbenateau@mailodie.fr>
2019-07-16 13:48:44 +0200

    change the read function to fread

--- tools/regionalgam/flight-curve.R
+++ tools/regionalgam/flight-curve.R
@@ -1,7 +1,12 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 source(args[1]) #TODO replace by library(regionalGAM) if available as official package from bioconda
-tryCatch({input = read.table(args[2], header=TRUE,sep="	")},finally={input = read.table(args[2], header=TRUE,sep=",")})
+library(data.table)
+# import file
+input = fread(args[2])
+# retransform to data.frame to allow the calculation by the fonctions from the package
+input = data.frame(input)
+# select only the columns with the specific names (specific format for the function)
 dataset1 <- input[,c("SPECIES", "SITE", "YEAR", "MONTH", "DAY", "COUNT")]
 pheno <- flight_curve(dataset1, MinVisit = args[3], MinOccur = args[4])
 
