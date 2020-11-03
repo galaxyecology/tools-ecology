95676e0003bfd5f8e481d6b47bfb79a7c6348952
ColineRoyaux <61148928+ColineRoyaux@users.noreply.github.com>
2020-11-03 16:54:20 +0100

    Typo on FunctExeCalcPresAbsGalaxy.r

--- tools/PAMPA/FunctExeCalcPresAbsGalaxy.r
+++ tools/PAMPA/FunctExeCalcPresAbsGalaxy.r
@@ -11,11 +11,11 @@ suppressMessages(library(tidyr))
 
 ###################### Load arguments and declaring variables
 
-args = commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
 
 if (length(args) < 2) {
-    stop("At least one argument must be supplied, an input dataset file (.tabular).", call.=FALSE) # if no args -> error and exit1
+    stop("At least one argument must be supplied, an input dataset file (.tabular).", call. = FALSE) # if no args -> error and exit1
 
 } else {
     import_data <- args[1] ###### Nom du fichier importé avec son extension / file name imported with the file type ".filetype"
@@ -26,10 +26,10 @@ if (length(args) < 2) {
 
 
 #Import des données / Import data
-obs<- read.table(import_data, sep="\t", dec=".", header=TRUE, encoding="UTF-8") #
+obs<- read.table(import_data, sep = "\t", dec = ".", header = TRUE, encoding = "UTF-8") #
 obs[obs == -999] <- NA
-factors <- fact_det_f(Obs=obs)
-obs_type <- def_typeobs_f(Obs=obs)
+factors <- fact_det_f(Obs = obs)
+obs_type <- def_typeobs_f(Obs = obs)
 obs <- create_unitobs(data = obs)
 
 vars_data <- c("observation.unit", "species.code", "number")
@@ -42,7 +42,7 @@ check_file(obs, err_msg_data, vars_data, 3)
 ####################################################################################################
 
 calc_pres_abs_f <- function(d_ata,
-                           nb_name="number") {
+                           nb_name = "number") {
     ## Purpose: Compute presence absence
     ## ----------------------------------------------------------------------
     ## Arguments: d_ata : temporary metrics table
@@ -63,11 +63,11 @@ calc_pres_abs_f <- function(d_ata,
 
 ################# Analysis
 
-res <- calc_numbers_f(obs, obs_type=obs_type, factors=factors, nb_name="number")
-res$presence_absence <- calc_pres_abs_f(res, nb_name="number")
+res <- calc_numbers_f(obs, obs_type = obs_type, factors = factors, nb_name = "number")
+res$presence_absence <- calc_pres_abs_f(res, nb_name = "number")
 res <- create_year_location(res)
 
 #Save dataframe in a tabular format
 filename_pres_abs <- "TabPresAbs.tabular"
-write.table(res, filename_pres_abs, row.names=FALSE, sep="\t", dec=".", fileEncoding="UTF-8")
-cat(paste("\nWrite table with presence/absence. \n--> \"", filename_pres_abs, "\"\n", sep=""))
+write.table(res, filename_pres_abs, row.names = FALSE, sep = "\t", dec = ".", fileEncoding = "UTF-8")
+cat(paste("\nWrite table with presence/absence. \n--> \"", filename_pres_abs, "\"\n", sep = ""))
