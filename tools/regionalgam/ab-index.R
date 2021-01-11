410a37ec0b4846c4e0eee9a47ebcb7861b2ffe89
sbenateau <sbenateau@mailodie.fr>
2021-01-11 23:15:34 +0100

    reformat R file ab-index

--- tools/regionalgam/ab-index.R
+++ tools/regionalgam/ab-index.R
@@ -1,13 +1,13 @@
 #!/usr/bin/env Rscript
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 source(args[1])
 
-input = data.frame(data.table::fread(args[2]))
-pheno = read.table(args[3], header=TRUE,sep="	")
+input <- data.frame(data.table::fread(args[2]))
+pheno <- read.table(args[3], header = TRUE, sep= "	")
 
-if("TREND" %in% colnames(input)){
-    input <- input[input$TREND==1,c("SPECIES","SITE","YEAR","MONTH","DAY","COUNT")]
+if ("TREND" %in% colnames(input)) {
+    input <- input[input$TREND == 1, c("SPECIES", "SITE", "YEAR", "MONTH", "DAY", "COUNT")]
 }
 data.index <- abundance_index(input, pheno)
 write.table(data.index, file="data.index", row.names=FALSE, sep="	")
