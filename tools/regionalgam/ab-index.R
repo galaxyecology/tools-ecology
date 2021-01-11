d5f9730e55d5a1818f7ff7e05717f0bc616c4c6e
sbenateau <sbenateau@mailodie.fr>
2021-01-12 00:01:09 +0100

    improve R script style

--- tools/regionalgam/ab-index.R
+++ tools/regionalgam/ab-index.R
@@ -4,10 +4,10 @@ args <- commandArgs(trailingOnly = TRUE)
 source(args[1])
 
 input <- data.frame(data.table::fread(args[2]))
-pheno <- read.table(args[3], header = TRUE, sep= "	")
+pheno <- read.table(args[3], header = TRUE, sep = "	")
 
 if ("TREND" %in% colnames(input)) {
     input <- input[input$TREND == 1, c("SPECIES", "SITE", "YEAR", "MONTH", "DAY", "COUNT")]
 }
-data.index <- abundance_index(input, pheno)
-write.table(data.index, file="data.index", row.names=FALSE, sep="	")
+data_index <- abundance_index(input, pheno)
+write.table(data.index, file = "data_index", row.names = FALSE, sep = "	")
