844667ec9fc67af0613e6b7d20d94efce239a170
sbenateau <sbenateau@mailodie.fr>
2021-01-12 01:20:53 +0100

    fix name error

--- tools/regionalgam/ab-index.R
+++ tools/regionalgam/ab-index.R
@@ -10,4 +10,4 @@ if ("TREND" %in% colnames(input)) {
     input <- input[input$TREND == 1, c("SPECIES", "SITE", "YEAR", "MONTH", "DAY", "COUNT")]
 }
 data_index <- abundance_index(input, pheno)
-write.table(data.index, file = "data_index", row.names = FALSE, sep = "	")
+write.table(data_index, file = "data_index", row.names = FALSE, sep = "	")
