b62389255af3827e925a7014bb0f76d6ddc2fde4
ColineRoyaux <61148928+ColineRoyaux@users.noreply.github.com>
2021-04-23 11:10:15 +0200

    remove quotes from output

--- tools/PAMPA/FunctExeCalcGLMSpGalaxy.r
+++ tools/PAMPA/FunctExeCalcGLMSpGalaxy.r
@@ -204,4 +204,4 @@ glm_species <- function(metrique, list_fact, list_rand, fact_ana, distrib, tab_m
 
 tab <- glm_species(metrique = metric, list_fact = list_fact, list_rand = list_rand, fact_ana = fact_ana, distrib = distrib, tab_metrics = obs, tab_metrique = aggreg, tab_unitobs = tab_unitobs, nb_name = "number")
 
-write.table(tab, "GLMSummary.tabular", row.names = FALSE, sep = "\t", dec = ".", fileEncoding = "UTF-8")
+write.table(tab, "GLMSummary.tabular", row.names = FALSE, quote = FALSE, sep = "\t", dec = ".", fileEncoding = "UTF-8")
