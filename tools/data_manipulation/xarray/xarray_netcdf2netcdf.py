6164f30eda769d6808addb7915021845a780d492
Anne Fouilloux <annefou@geo.uio.no>
2022-01-06 22:23:58 +0000

    select by values

--- tools/data_manipulation/xarray/xarray_netcdf2netcdf.py
+++ tools/data_manipulation/xarray/xarray_netcdf2netcdf.py
@@ -23,7 +23,9 @@ class netCDF2netCDF ():
                  write_all=False,
                  keep_attributes=True,
                  filter_list="",
+                 sel=False,
                  verbose=False):
+        self.sel = sel
         li = list(infile.split(","))
         if len(li) > 1:
             self.infile = li
@@ -59,15 +61,22 @@ class netCDF2netCDF ():
             print("scale: ", self.scale)
             print("write_all: ", self.write_all)
             print("keep_attributes: ", self.keep_attributes)
+            print("sel: ", self.sel)
             print("output: ", self.output)
 
     def dimension_selection(self, single_filter):
         split_filter = single_filter.split('#')
         dimension_varname = split_filter[0]
         op = split_filter[1]
-        ll = int(split_filter[2])
+        if self.sel:
+            ll = float(split_filter[2])
+        else:
+            ll = int(split_filter[2])
         if (op == 'sl'):
-            rl = int(split_filter[3])
+            if self.sel:
+                rl = float(split_filter[3])
+            else:
+                rl = int(split_filter[3])
             self.selection[dimension_varname] = slice(ll, rl)
         elif (op == 'to'):
             self.selection[dimension_varname] = slice(None, ll)
@@ -75,23 +84,38 @@ class netCDF2netCDF ():
             self.selection[dimension_varname] = slice(ll, None)
         elif (op == 'is'):
             self.selection[dimension_varname] = ll
+            if self.sel:
+                rl = split_filter[3]      
+                self.selection['method'] = r1
 
     def filter_selection(self):
         for single_filter in self.filter:
             self.dimension_selection(single_filter)
 
         if self.varname == 'None' or self.varname is None:
-            self.dset = \
-                self.ds.isel(self.selection)
-        else:
-            if self.write_all:
-                for var in self.varname:
-                    self.ds[var] = \
-                        self.ds[var].isel(self.selection)*self.scale
-                self.dset = self.ds
+            if self.sel:
+                self.dset = \
+                    self.ds.sel(self.selection)
             else:
                 self.dset = \
                     self.ds.isel(self.selection)
+        else:
+            if self.write_all:
+                for var in self.varname:     
+                    if self.sel:      
+                        self.ds[var] = \
+                            self.ds[var].sel(self.selection)*self.scale  
+                    else:      
+                        self.ds[var] = \
+                            self.ds[var].isel(self.selection)*self.scale
+                self.dset = self.ds
+            else: 
+                if self.sel:      
+                    self.dset = \
+                        self.ds.sel(self.selection)
+                else:      
+                    self.dset = \
+                        self.ds.isel(self.selection)
                 for var in self.varname:
                     self.dset[var] = \
                         self.dset[var]*self.scale
@@ -151,11 +175,17 @@ if __name__ == '__main__':
         "-v", "--verbose",
         help="switch on verbose mode",
         action="store_true")
+    parser.add_argument(
+        "--selection",
+        help="select by values",
+        action="store_true")
     args = parser.parse_args()
 
+    print("args.selection", args.selection)
     dset = netCDF2netCDF(infile=args.input, varname=args.varname,
                          scale=args.scale, output=args.output,
                          write_all=args.write_all,
+                         sel=args.selection,
                          keep_attributes=args.keep_attributes,
                          filter_list=args.filter,
                          verbose=args.verbose)
