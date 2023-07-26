a17ce638c156468ccacf9df2122277b387f26f14
TanguyGen <72631088+TanguyGen@users.noreply.github.com>
2023-07-26 17:03:35 +0200

    Update make_eml.R

--- tools/EMLassemblyline/make_eml.R
+++ tools/EMLassemblyline/make_eml.R
@@ -2,5 +2,12 @@
 ##Genthon Tanguy
 ###make_eml
 
-EMLassemblyline::make_eml(".")
+args = commandArgs(trailingOnly=TRUE)
+if(length(args)>0){
+  title <- args[1]
+}
 
+EMLassemblyline::make_eml("output_template",eml.path=".", dataset.title = title)
+old.names <- list.files(path=".", pattern=".xml")
+print(old.names)
+file.rename(from=old.names, to="eml.xml")
