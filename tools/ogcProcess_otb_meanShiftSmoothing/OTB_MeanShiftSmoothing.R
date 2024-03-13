00cdbf3074601ace07d70dd4f9c8c1061382d5ea
MarkusKonk <m.konkol@52north.org>
2024-03-13 12:10:36 +0100

    lint r script

--- tools/ogcProcess_otb_meanShiftSmoothing/OTB_MeanShiftSmoothing.R
+++ tools/ogcProcess_otb_meanShiftSmoothing/OTB_MeanShiftSmoothing.R
@@ -1,4 +1,9 @@
-#Run with Rscript ./OTB_MeanShiftSmoothing.R --file otb_band_math_test_input.txt --fOut float --fOutpos float --processingMemory 1024 --spatialR 5 --rangeR 15 --thresHold 0.1 --maxIter 100 --rangeRamp 0 --modeSearch False --outputType png --outputFormat download --outputData test1.png
+#Run with Rscript ./OTB_MeanShiftSmoothing.R
+#--file otb_band_math_test_input.txt
+#--fOut float --fOutpos float --processingMemory 1024 --spatialR 5 --rangeR 15
+#--thresHold 0.1 --maxIter 100 --rangeRamp 0 --modeSearch False
+#--outputType png
+#--outputFormat download --outputData test1.png
 
 library("httr2")
 library("jsonlite")
@@ -6,19 +11,19 @@ library("getopt")
 
 args <- commandArgs(trailingOnly = TRUE)
 option_specification <- matrix(c(
-  'file', 'i1', 1, 'character',
-  'fOut', 'i2', 1, 'character',
-  'fOutpos', 'i3', 1, 'character',
-  'processingMemory', 'i4', 1, 'integer',
-  'spatialR', 'i5', 2, 'integer',
-  'rangeR', 'i6', 2, 'double',
-  'thresHold', 'i7', 2, 'double',
-  'maxIter', 'i8', 2, 'integer',
-  'rangeRamp', 'i9', 2, 'double',
-  'modeSearch', 'i10', 1, 'character',
-  'outputType', 'i11', 1, 'character',
-  'outputFormat', 'i12', 1, 'character',
-  'outputData', 'o', 1, 'character'
+  "file", "i1", 1, "character",
+  "fOut", "i2", 1, "character",
+  "fOutpos", "i3", 1, "character",
+  "processingMemory", "i4", 1, "integer",
+  "spatialR", "i5", 2, "integer",
+  "rangeR", "i6", 2, "double",
+  "thresHold", "i7", 2, "double",
+  "maxIter", "i8", 2, "integer",
+  "rangeRamp", "i9", 2, "double",
+  "modeSearch", "i10", 1, "character",
+  "outputType", "i11", 1, "character",
+  "outputFormat", "i12", 1, "character",
+  "outputData", "o", 1, "character"
 ), byrow = TRUE, ncol = 4)
 options <- getopt(option_specification)
 
@@ -84,7 +89,7 @@ json_data <- list(
       "format" = list(
         "mediaType" = outputType
       ),
-      "transmissionMode"= "reference"
+      "transmissionMode" = "reference"
     )
   )
 )
@@ -102,24 +107,22 @@ tryCatch({
   #Request 1
   resp1 <- request(paste0(baseUrl, execute)) %>%
     req_headers(
-      'accept' = '/*',
-      'Prefer' = 'respond-async;return=representation',
-      'Content-Type' = 'application/json'
+      "accept" = "/*",
+      "Prefer" = "respond-async;return=representation",
+      "Content-Type" = "application/json"
     ) %>%
     req_body_json(json_data) %>%
     req_perform()
-  
   response <- makeResponseBodyReadable(resp1$body)
   status_code1 <- resp1$status_code
-  
   if (status_code1 == 201) {
     status <- "running"
-    attempt = 1
+    attempt <- 1
     while (status == "running") {
       #Request 2
       resp2 <- request(paste0(baseUrl,getStatus,response$jobID)) %>%
         req_headers(
-          'accept' = 'application/json'
+          "accept" = "application/json"
         ) %>%
         req_perform()
       status_code2 <- resp2$status_code
@@ -131,7 +134,7 @@ tryCatch({
           #Request 3
           resp3 <- request(paste0(baseUrl,getStatus, response2$jobID, getResult)) %>%
             req_headers(
-              'accept' = 'application/json'
+              "accept" = "application/json"
             ) %>%
             req_perform()
           status_code3 <- resp3$status_code
@@ -140,7 +143,7 @@ tryCatch({
             if (outputFormat == "download") {
               options(timeout=600)
               download.file(response3$fout$href, destfile = paste0("output1.", options$outputType), mode = "wb")
-              download.file(response3$foutpos$href, destfile = paste0("output2.", options$outputType), mode = "wb")              
+              download.file(response3$foutpos$href, destfile = paste0("output2.", options$outputType), mode = "wb")
             } else if (outputFormat == "getUrl") {
               writeLines(paste(response3$fout$href, response3$foutpos$href, sep = "\n"), con = "output.txt")
             }
@@ -153,13 +156,13 @@ tryCatch({
           }
         } else if (response2$status=="failed") {
           status <- "failed"
-        } 
-         #else {
-         # attempt <- attempt +1
-         # if (attempt == 200) {
-         #   status <- "failed"          
-         # }
-        #}        
+        }
+        #else {
+        # attempt <- attempt +1
+        # if (attempt == 200) {
+        #   status <- "failed"
+        # }
+        #}
       } else {
         status <- "failed"
         print(paste("HTTP", status_code2, "Error:", resp2$status_message))
