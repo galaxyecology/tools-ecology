cca63b17cc1053879c0fa3f936f91dfd91971ff5
MarkusKonk <m.konkol@52north.org>
2024-05-14 17:00:30 +0200

    some linting

--- tools/aquainfra_importer/data_source.py
+++ tools/aquainfra_importer/data_source.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-#Retrieves data from external data source applications and 
-#stores in a dataset file.
+# Retrieves data from external data source applications and
+# stores in a dataset file.
 #
-#Data source application parameters are temporarily stored 
-#in the dataset file.
+# Data source application parameters are temporarily stored
+# in the dataset file.
 import json
 import os
 import sys
@@ -50,12 +50,14 @@ def main():
     for data_dict in params["output_data"]:
         cur_filename = data_dict["file_name"]
         cur_URL = params["param_dict"].get(
-            "%s|%s|URL" % (GALAXY_PARAM_PREFIX, data_dict["out_data_name"]), URL
+            "%s|%s|URL" % (GALAXY_PARAM_PREFIX, data_dict["out_data_name"]), 
+                URL
         )
-        if not cur_URL or urlparse(cur_URL).scheme not in ("http", "https", "ftp"):
+        if not cur_URL or urlparse(cur_URL).scheme not in ("http", "https", 
+            "ftp"):
             open(cur_filename, "w").write("")
             sys.exit(
-                "The remote data source application has not sent "\
+                "The remote data source application has not sent "
                 "back a URL parameter in the request."
             )
 
@@ -72,7 +74,7 @@ def main():
                 )
         except Exception as e:
             sys.exit(
-                "The remote data source application may be off line, please try "\
+                "The remote data source application may be off line, please try "
                 "again later. Error: %s"
                 % str(e)
             )
@@ -80,7 +82,7 @@ def main():
             file_size = int(page.info().get("Content-Length", 0))
             if file_size > max_file_size:
                 sys.exit(
-                    "The requested data size (%d bytes) exceeds the maximum"\
+                    "The requested data size (%d bytes) exceeds the maximum"
                     "allowed size (%d bytes) on this server."
                     % (file_size, max_file_size)
                 )
