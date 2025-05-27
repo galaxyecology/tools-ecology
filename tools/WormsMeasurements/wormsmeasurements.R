874c3f5ab62cfdeec98845b71dbb0d9d41503be9
JinsNoe <jinsnoe@gmail.com>
2025-05-27 15:51:57 +0200

    added regex to keep genus and specific epithet, comments, fixed mutate(accross(all_of())) error happening when no values a measurement type was found

--- tools/WormsMeasurements/wormsmeasurements.R
+++ tools/WormsMeasurements/wormsmeasurements.R
@@ -13,15 +13,19 @@ if (length(args) == 0) {
     stop("This tool needs at least one argument")
 }
 
-occurrence <- read.csv(args[1], header=T, sep="\t") %>% arrange(scientificName)
+scientificName_name <- args[3]
+occurrence <- read.csv(args[1], header=T, sep="\t") %>% 
+  arrange(.[[scientificName_name]])
 measurement_types <- unlist(str_split(args[2], ","))
 include_inherited <- ifelse(args[4]=="true", T, F)
 pivot_wider <- ifelse(args[5]=="true", T, F)
-scientificName_name <- args[3]
 exclude_NA <- ifelse(args[6]=="true", T, F)
 
+regex_find <- "^([A-Z][a-z]+)\\s+([a-z-]+).*"
+regex_replace <- "\\1 \\2"
 
-### 
+
+# function to extract the measurement values from the attributes data tibble
 extract_traits_values <- function(traits_data) {
   result <- setNames(rep(NA, length(measurement_types)), measurement_types)
   
@@ -43,18 +47,21 @@ extract_traits_values <- function(traits_data) {
   return(result)
 }
 
+# function to call the call the WoRMS API and get the measurement values
 get_life_history_traits <- function(scientific_name) {
-  if (scientific_name %in% names(cache)) { 
-    return(cache[[scientific_name]])  
+  clean_scientific_name <- gsub(regex_find, regex_replace, scientific_name)
+
+  if (clean_scientific_name %in% names(cache)) { 
+    return(cache[[clean_scientific_name]])  
   }
   
   worms_id <- tryCatch(
-    wm_name2id(name = scientific_name),
+    wm_name2id(name = clean_scientific_name),
     error = function(e) NA
   )
   
   if (is.na(worms_id) || length(worms_id) == 0) {
-    cache[[scientific_name]] <<- NULL
+    cache[[clean_scientific_name]] <<- NULL
     return(NULL)
   }
   
@@ -64,25 +71,37 @@ get_life_history_traits <- function(scientific_name) {
   )
   
   if (is.null(data_attr)) {
-    cache[[scientific_name]] <<- NULL
+    cache[[clean_scientific_name]] <<- NULL
     return(NULL)
   }
   
   traits <- extract_traits_values(data_attr)
-  cache[[scientific_name]] <<- traits
+  cache[[clean_scientific_name]] <<- traits
   return(traits)
 }
 
+# a cache to limit API calls
 cache <- list()
 
+# add a columns conataining the lists of values of the measurments requested
 trait_data <- occurrence %>%
   mutate(life_history_traits = map(.data[[scientificName_name]], ~ get_life_history_traits(.x)))
 
+# convert the column of lists to multiple columns of unique values
 trait_data <- trait_data %>%
   unnest_wider(life_history_traits)
 
+# make sur each measurement type has a column
+for (col in measurement_types) {
+  if (!(col %in% names(trait_data))) {
+    trait_data[[col]] <- NA
+  }
+}
+
+# list of quantitativ measurements
 numeric_cols <- c()
 
+# try to convert columns to numeric and remember them
 trait_data <- trait_data %>%
   mutate(across(all_of(measurement_types), ~ {
     numeric_col <- suppressWarnings(as.numeric(.))
@@ -94,14 +113,18 @@ trait_data <- trait_data %>%
     }
   }))
 
+# filter NA but only in the added columns
 if (exclude_NA) {
   trait_data <- trait_data[complete.cases(trait_data[, measurement_types]),]
 }
 
-factor_cols = setdiff(measurement_types, numeric_cols)
+# determine what are the qualitativ columns to be one hot encoded
+factor_cols <- setdiff(measurement_types, numeric_cols)
 
+# one hot encode quantitativ columns
 if (pivot_wider & length(factor_cols) > 0) {
   trait_data <- dummy_cols(trait_data, select_columns = factor_cols, remove_selected_columns=T, ignore_na=T)
 }
 
+# write the enriched dataset as tabular
 write.table(trait_data, "enriched_data.tabular", sep="\t", row.names = FALSE)
