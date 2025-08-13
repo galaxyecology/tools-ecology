3988d34e5938182e97dc3e2181b736b8866e1538
Kim Leirvik <kim.leirvik@niva.no>
2025-08-13 12:46:12 +0200

    fix python linting

--- tools/xarray_import_data/xarray_import_data.py
+++ tools/xarray_import_data/xarray_import_data.py
@@ -7,21 +7,29 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "opendap_url",
-    help="A valid OPeNDAP URL, also see https://docs.xarray.dev/en/stable/user-guide/io.html#opendap",
+    help=(
+        "A valid OPeNDAP URL, also see "
+        "https://docs.xarray.dev/en/stable/user-guide/io.html#opendap"
+    ),
 )
 parser.add_argument(
     "decode_times",
-    type=lambda x: x=="true",
+    type=lambda x: x == "true",
     help='If time should be decoded, e.g. "True" or "False"',
 )
 parser.add_argument(
     "decode_cf",
-    type=lambda x: x=="true",
-    help='Whether to decode these variables, assuming they were saved according to CF conventions e.g. "True" or "False"',
+    type=lambda x: x == "true",
+    help=(
+        "Whether to decode according to "
+        'CF conventions e.g. "true" or "false"'
+    ),
 )
 parser.add_argument("output_dataset", help="netCDF file to output")
 args = parser.parse_args()
 
 xr.open_dataset(
-    args.opendap_url.strip(), decode_cf=args.decode_cf, decode_times=args.decode_times
+    args.opendap_url.strip(),
+    decode_cf=args.decode_cf,
+    decode_times=args.decode_times,
 ).to_netcdf(args.output_dataset)
